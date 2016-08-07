ca866d3ee2a561bf18f1393ac3fcf3b0e035ab59
Meksavanh <ventialaw@hotmail.fr>
2016-08-07 18:34:26 +0200

    Teddy : scroll Object, navbar Object

--- simple-todos/app-home/client/view/home.js
+++ simple-todos/app-home/client/view/home.js
@@ -1,25 +1,72 @@
 Template.home.onCreated(() => {
-    var i = 1;
+    "use strict";
 
-    $(document).on("keydown", function (event) {
-
-        if (event.keyCode == 40) {
-            i++;
-            if(i > 4)
-                i = 4;
-            scrollToAnchor($("#section"+i));
-        } else if (event.keyCode == 38) {
-            i--;
-            if(i < 1)
-                i = 1;
-            event.preventDefault();
-            scrollToAnchor($("#section"+i));
-        }
+    window.addEventListener('load', function load(){
+        window.removeEventListener('load', load, false);
+        navbar.init();
+        scroll.init();
     });
 
-    function scrollToAnchor(aid){
-        $('html,body').animate({scrollTop: aid.offset().top},'slow');
-    }
+    var navbar = {
+        init : function(){
+            navbar.setNavbar();
+            navbar.isActive();
+        },
+        //Setter
+        setNavbar : function(){
+            this._navbar = jQuery('.portfolio-navbar');
+        },
+        //Getter
+        getNavbar : function(){
+            return this._navbar;
+        },
+        isActive : function(){
+            navbar.getNavbar().find('li a').click(function(e){
+                navbar.getNavbar().find('li').removeClass('active');
+                var thisParent = jQuery(this).parent();
+                if(!thisParent.hasClass('active'))
+                    thisParent.addClass('active');
+            });
+        }
+    };
 
-    console.log(i);
+    var scroll = {
+        init : function(){
+            scroll.defaultTop();
+            scroll.goToSection(4);
+        },
+        //Setter
+
+        //Getter
+
+        defaultTop : function(){
+            jQuery(window).on('beforeunload', function() {
+                jQuery(window).scrollTop(0);
+            });
+        },
+        goToSection : function(nbSection){
+            var i = 1;
+
+            jQuery(document).on("keydown", function (event) {
+
+                if (event.keyCode == 40) {
+                    if(i>=nbSection)
+                        i=nbSection;
+                    else
+                        i++;
+                    navbar.scrollToAnchor(jQuery("#section"+i));
+                } else if (event.keyCode == 38) {
+                    if(i<=1)
+                        i=1;
+                    else
+                        i--;
+                    event.preventDefault();
+                    navbar.scrollToAnchor(jQuery("#section"+i));
+                }
+            });
+        },
+        scrollToAnchor : function(selectr){
+            jQuery('html,body').animate({scrollTop: selector.offset().top},'slow');
+        }
+    };
 });
