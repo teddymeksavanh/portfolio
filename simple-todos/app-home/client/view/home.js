f1aed9824b945359d72ad5dc6fd7583187f771b3
Meksavanh <ventialaw@hotmail.fr>
2016-08-07 15:55:52 +0200

    Teddy : Ajout d'un scroll sur Key

---
+++ simple-todos/app-home/client/view/home.js
@@ -0,0 +1,25 @@
+Template.home.onCreated(() => {
+    var i = 1;
+
+    $(document).on("keydown", function (event) {
+
+        if (event.keyCode == 40) {
+            i++;
+            if(i > 3)
+                i = 3;
+            scrollToAnchor($("#section"+i));
+        } else if (event.keyCode == 38) {
+            i--;
+            if(i < 1)
+                i = 1;
+            event.preventDefault();
+            scrollToAnchor($("#section"+i));
+        }
+    });
+
+    function scrollToAnchor(aid){
+        $('html,body').animate({scrollTop: aid.offset().top},'slow');
+    }
+
+    console.log(i);
+});
